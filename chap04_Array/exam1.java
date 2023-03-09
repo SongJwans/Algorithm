4d7a346b246778896db0d9698194fefb1bac27cb
SongJwans <yhh5916@naver.com>
2023-03-09 23:40:46 +0900

    chap04_Array

---
+++ chap04_Array/exam1.java
@@ -0,0 +1,10 @@
+package chap04_Array;
+
+public class exam1 {
+    public static void main(String[] args) {
+        Integer[][][] data_list = {{{1, 2, 3}, {4, 5, 6}}, {{7, 8, 9}, {10, 11, 12}}};
+        System.out.println(data_list[1][0][1]);
+        System.out.println(data_list[1][1][0]);
+        System.out.println(data_list[0][0][1]);
+    }
+}
